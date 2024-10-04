b750c20db70514ea58c150e725aa5fd192b365df
Robocoders <team@blackbox.ai>
2024-10-04 05:02:01 +0000

    Update routes and add redirects for Netlify and GitHub Pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,12 @@ import { Routes } from '@angular/router';
 
 import { CreatePartyComponent } from './create-party/create-party.component';
 import { loadComponent } from '@angular/core';
+import { ProfitLossComponent } from './profit-loss/profit-loss.component';
+import { TodaysTransactionsComponent } from './todays-transactions/todays-transactions.component';
 
 export const routes: Routes = [
   { path: 'create-party', component: CreatePartyComponent },
-  { path: 'acc-transaction', loadComponent: () => import('./acc-transaction/acc-transaction.component').then(m => m.AccTransactionComponent) }
+  { path: 'acc-transaction', loadComponent: () => import('./acc-transaction/acc-transaction.component').then(m => m.AccTransactionComponent) },
+  { path: 'profit-loss', component: ProfitLossComponent },
+  { path: 'todays-transactions', component: TodaysTransactionsComponent }
 ];
